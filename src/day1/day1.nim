24d63c23ff21ba3af1cec3fd0e2919d5a1dd2c2a
Nathan Barlow <nathanjbarlow@gmail.com>
2022-12-03 01:08:58 -0600

    Write functional solution to day1

--- src/day1/day1.nim
+++ src/day1/day1.nim
@@ -1,5 +1,4 @@
-import strutils
-import std/algorithm
+import strutils, algorithm, sequtils, sugar, math
 
 proc getElves(file: string): seq[int] = 
     var elf = 0
@@ -10,9 +9,22 @@ proc getElves(file: string): seq[int] =
         else:
             elf += line.parseInt()
 
+proc getFunctionalElves(file: string): seq[int] =
+    readFile(file)
+     .split("\r\n\r\n")
+     .map((elf) =>
+         elf.split("\r\n")
+         .map(parseInt)
+         .sum())
+
 proc dayOne*(file: string) =
     var elves = getElves(file)
     echo "Part 1: ", max(elves)
     elves.sort()
     echo "Part 1 but sorted: ", elves[^1]
     echo "Part 2: ", elves[^1] + elves[^2] + elves[^3]
+
+    elves = getFunctionalElves(file)
+    elves.sort()
+    echo "Functional Part 1: ", elves[^1]
+    echo "Functional Part 2: ", elves[^1] + elves[^2] + elves[^3]
