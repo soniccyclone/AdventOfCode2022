39b36c943d99106ef18885e0cd1b85d94ab2a353
Nathan Barlow <nathanjbarlow@gmail.com>
2022-12-03 11:21:04 -0600

    use funcs instead of procs

--- src/day2/day2.nim
+++ src/day2/day2.nim
@@ -1,16 +1,16 @@
 import std/strutils, sugar
 
-proc isTie(opponent, you: string): bool =
+func isTie(opponent, you: string): bool =
     (opponent == "A" and you == "X") or
     (opponent == "B" and you == "Y") or
     (opponent == "C" and you == "Z")
 
-proc isVictory(opponent, you: string): bool =
+func isVictory(opponent, you: string): bool =
     (opponent == "C" and you == "X") or
     (opponent == "A" and you == "Y") or
     (opponent == "B" and you == "Z")
 
-proc getScore(opponent, you: string): int =
+func getScore(opponent, you: string): int =
     if isTie(opponent, you):
         result += 3
     elif isVictory(opponent, you):
@@ -24,7 +24,7 @@ proc getScore(opponent, you: string): int =
     of "Z":
         result += 3
 
-proc getPredestinedScore(opponent, outcome: string): int =
+func getPredestinedScore(opponent, outcome: string): int =
     case outcome:
     of "X":
         #loss
