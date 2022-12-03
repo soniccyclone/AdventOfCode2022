e6333dfba2c7c94230724435b77b133d39d9c158
Nathan Barlow <nathanjbarlow@gmail.com>
2022-12-03 00:43:52 -0600

    Refactor day1

--- src/day1/day1.nim
+++ src/day1/day1.nim
@@ -2,15 +2,13 @@ import strutils
 import std/algorithm
 
 proc getElves(file: string): seq[int] = 
-    let f = open(file)
     var elf = 0
-    for line in f.lines:
+    for line in open(file).lines:
         if line == "":
             result.add(elf)
             elf = 0
         else:
             elf += line.parseInt()
-    f.close()
 
 proc dayOne*(file: string) =
     var elves = getElves(file)
