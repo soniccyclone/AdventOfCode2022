45c1240eca69086620c9bb6ed8e1170e36d7ad38
Nathan Barlow <nathanjbarlow@gmail.com>
2022-12-03 13:25:19 -0600

    Refactor day3 with distribute

--- src/advent/day3.nim
+++ src/advent/day3.nim
@@ -8,34 +8,30 @@ const priority = toSeq('a'..'z').concat(toSeq('A'..'Z'))
 const priorityLookup = zip(priority, toSeq(1..priority.len)).toTable
 
 func getRucksackTotal(rucksack: string): int =
-    let compartmentOne = toHashSet(rucksack[0..<rucksack.len.floorDiv(2)])
-    let compartmentTwo = toHashSet(rucksack[rucksack.len.floorDiv(2)..^1])
-    let sharedItems = compartmentOne * compartmentTwo
-    sharedItems.map((item) => priorityLookup[item])
+    rucksack.toSeq.distribute(2)
+    .map(compartment => compartment.toHashSet)
+    .foldl(a * b)
+    .map((item) => priorityLookup[item])
     .toSeq
     .sum
 
 func getPrioritySum(file: string): int =
-        file.split("\r\n")
-        .map((rucksack) => getRucksackTotal(rucksack))
-        .sum
+    file.split("\r\n")
+    .map((rucksack) => getRucksackTotal(rucksack))
+    .sum
 
-func getBadgeSum(file: string): int =
-    let lines = file.split("\r\n")
-    # TODO: Write a macro for map that returns sequences of items based off of a step
-    for i in countup(0, lines.len - 1, 3):
-        # Assume file will always be divisible by three to form proper elven groups
-        let elfOne = toHashSet(lines[i])
-        let elfTwo = toHashSet(lines[i + 1])
-        let elfThree = toHashSet(lines[i + 2])
-        var badge =  elfOne * elfTwo * elfThree
-        result += priorityLookup[badge.pop]
+func getBadgeSum(lines: seq[string]): int =
+    lines.distribute(lines.len.floorDiv(3))
+    .map(elfGroup => priorityLookup[
+            (toHashSet(elfGroup[0]) * toHashSet(elfGroup[1]) * toHashSet(elfGroup[2])).toSeq[0]
+        ])
+    .sum
 
 proc dayThree*(fileName: string) =
     echo "Day 3 Part 1: ", getPrioritySum(readFile(fileName))
-    echo "Day 3 Part 2: ", getBadgeSum(readFile(fileName))
+    echo "Day 3 Part 2: ", getBadgeSum(readFile(fileName).split("\r\n"))
 
 when isMainModule:
     assert getPrioritySum(readFile("src/input/day3.test")) == 157
-    assert getBadgeSum(readFile("src/input/day3.test")) == 70
+    assert getBadgeSum(readFile("src/input/day3.test").split("\r\n")) == 70
     echo "All tests passed!"
