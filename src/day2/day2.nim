431e1fca595c86478dabd915cfde30e1b12195c7
Nathan Barlow <nathanjbarlow@gmail.com>
2022-12-03 00:32:21 -0600

    Complete day 2 part 2

--- src/day2/day2.nim
+++ src/day2/day2.nim
@@ -28,11 +28,49 @@ proc getTotalScore(file: File): int =
         let plays = line.split(' ')
         result += getScore(plays[0], plays[1])
 
+
+proc getPredestinedScore(opponent, outcome: string): int =
+    case outcome:
+    of "X":
+        #loss
+        case opponent:
+        of "A":
+            result += 3
+        of "B":
+            result += 1
+        of "C":
+            result += 2
+    of "Y":
+        #draw
+        result += 3
+        case opponent:
+        of "A":
+            result += 1
+        of "B":
+            result += 2
+        of "C":
+            result += 3
+    of "Z":
+        #win
+        result += 6
+        case opponent:
+        of "A":
+            result += 2
+        of "B":
+            result += 3
+        of "C":
+            result += 1
+
+proc getTotalPredestinedScore(file: File): int =
+    for line in file.lines:
+        var plays = line.split(' ')
+        result += getPredestinedScore(plays[0], plays[1])
+
 proc dayTwo*(fileName: string) =
     echo "Day 2 Part 1: ", getTotalScore(open(fileName))
+    echo "Day 2 Part 2: ", getTotalPredestinedScore(open(fileName))
 
 when isMainModule:
-    block:
-        let data = open("src/day2/test.in")
-        assert getTotalScore(data) == 15
-        echo "All tests passed!"
+    assert getTotalScore(open("src/day2/test.in")) == 15
+    assert getTotalPredestinedScore(open("src/day2/test.in")) == 12
+    echo "All tests passed!"
