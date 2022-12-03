6a94c505d22d5abbbd23804ddd0d5dad4c415bc7
Nathan Barlow <nathanjbarlow@gmail.com>
2022-12-03 00:10:50 -0600

    Complete day 2 part 1

--- src/day2/day2.nim
+++ src/day2/day2.nim
@@ -1,9 +1,38 @@
+import std/strutils
 
-proc getScore(file: File): int =
-    0
+proc isTie(opponent, you: string): bool =
+    (opponent == "A" and you == "X") or
+    (opponent == "B" and you == "Y") or
+    (opponent == "C" and you == "Z")
+
+proc isVictory(opponent, you: string): bool =
+    (opponent == "C" and you == "X") or
+    (opponent == "A" and you == "Y") or
+    (opponent == "B" and you == "Z")
+
+proc getScore(opponent, you: string): int =
+    if isTie(opponent, you):
+        result += 3
+    elif isVictory(opponent, you):
+        result += 6
+    
+    if you == "X":
+        result += 1
+    elif you == "Y":
+        result += 2
+    else:
+        result += 3
+
+proc getTotalScore(file: File): int =
+    for line in file.lines:
+        let plays = line.split(' ')
+        result += getScore(plays[0], plays[1])
+
+proc dayTwo*(fileName: string) =
+    echo "Day 2 Part 1: ", getTotalScore(open(fileName))
 
 when isMainModule:
     block:
         let data = open("src/day2/test.in")
-        assert getScore(data) == 15
+        assert getTotalScore(data) == 15
         echo "All tests passed!"
--- src/main.nim
+++ src/main.nim
@@ -1,3 +1,5 @@
-import day1/day1
+import day1/day1,
+    day2/day2
 
 dayOne("src/day1/day1.in")
+dayTwo("src/day2/day2.in")
