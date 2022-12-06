8062d3be4eb06761d7cd796b1a62828f7d02690b
Nathan Barlow <nathanjbarlow@gmail.com>
2022-12-06 08:53:45 -0600

    Make failed attempt at day 4 part 1, maybe I should just use sets...

--- src/main.nim
+++ src/main.nim
@@ -1,5 +1,6 @@
-import advent/[day1, day2, day3]
+import advent/[day1, day2, day3, day4]
 
 dayOne("src/input/day1.in")
 dayTwo("src/input/day2.in")
 dayThree("src/input/day3.in")
+dayFour("src/input/day4.in")
