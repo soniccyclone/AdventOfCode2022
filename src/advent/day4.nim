8144b34f487d9c35860263059513bb086504b8eb
Nathan Barlow <nathanjbarlow@gmail.com>
2022-12-06 18:58:43 -0600

    Complete day 4 part 2

--- src/advent/day4.nim
+++ src/advent/day4.nim
@@ -1,4 +1,4 @@
-import sequtils, strutils, system/iterators
+import sequtils, strutils, system/iterators, std/strformat
 
 func elfBounds(elf: string): tuple[start, finish: int] =
     let bounds = elf.split("-").map(parseInt)
@@ -13,9 +13,22 @@ func isAssignmentContained(elfGroup: string): bool =
 func sharedUniverses(line: seq[string]): int =
     line.map(isAssignmentContained).filterIt(it).len
 
+func isOverlapped(elfGroup: string): bool =
+    let elves = elfGroup.split(",").map(elfBounds)
+
+    (elves[0].start <= elves[1].finish) and
+    (elves[1].start <= elves[0].finish)
+
+func assignmentsOverlapped(lines: seq[string]): int =
+    lines.map(isOverlapped).filterIt(it).len
+
 proc dayFour*(fileName: string) =
     echo "Day 4 Part 1: ", sharedUniverses(readFile(fileName).split("\r\n"))
+    echo "Day 4 Part 2: ", assignmentsOverlapped(readFile(fileName).split("\r\n"))
 
 when isMainModule:
-    assert sharedUniverses(readFile("src/input/day4.test").split("\r\n")) == 2
+    let part1 = sharedUniverses(readFile("src/input/day4.test").split("\r\n"))
+    assert part1 == 2, &"Expected 2, got {part1}"
+    let part2 = assignmentsOverlapped(readFile("src/input/day4.test").split("\r\n"))
+    assert part2 == 4, &"Expected 4, got {part2}"
     echo "All tests passed!"
