7ffb1f8c8e77c0140e12ba628a6592f3059ccffa
Nathan Barlow <nathanjbarlow@gmail.com>
2022-12-06 18:29:20 -0600

    Complete day 4 part 1

--- src/advent/day4.nim
+++ src/advent/day4.nim
@@ -1,16 +1,20 @@
-import math, sets, sequtils, tables, sugar, strutils, system/iterators
+import sequtils, strutils, system/iterators
 
 func isAssignmentContained(elfGroup: string): bool =
     let elves = elfGroup.split(",")
+
     let elf1 = elves[0]
     let elf2 = elves[1]
-    let elf1Bounds = elf1.split("-")
+
+    let elf1Bounds = elf1.split("-").map(parseInt)
     let elf1Start = elf1Bounds[0]
     let elf1End = elf1Bounds[1]
-    let elf2Bounds = elf2.split("-")
+
+    let elf2Bounds = elf2.split("-").map(parseInt)
     let elf2Start = elf2Bounds[0]
     let elf2End = elf2Bounds[1]
-    (elf1Start <= elf2Start and elf1End >= elf2End) or (elf2Start < elf1Start and elf2End > elf1End)
+    
+    (elf1Start <= elf2Start and elf1End >= elf2End) or (elf2Start <= elf1Start and elf2End >= elf1End)
 
 func sharedUniverses(line: seq[string]): int =
     line.map(isAssignmentContained).filterIt(it).len
